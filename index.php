7dd2c68134a9fef3bffc137d6792ce0cca3ca6b2
Alexander Matheisen <AlexanderMatheisen@ish.de>
2017-01-07 23:18:09 +0100

    Removed Piwik tracking code, no longer used

--- index.php
+++ index.php
@@ -28,24 +28,6 @@
 		<meta name="page-topic" content="OpenRailwayMap" />
 		<meta name="robots" content="index,follow" />
 		<link rel="alternate" type="application/rss+xml" title="OpenRailwayMap RSS Feed" href="http://blog.openrailwaymap.org/<? echo $lang; ?>.rss" />
-		<!-- Piwik -->
-		<script type="text/javascript">
-			var _paq = _paq || [];
-			_paq.push(['trackPageView']);
-			_paq.push(['enableLinkTracking']);
-			(function()
-			{
-				var u="//piwik.openrailwaymap.org/";
-				_paq.push(['setTrackerUrl', u+'piwik.php']);
-				_paq.push(['setSiteId', 6]);
-				var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-				g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
-			})();
-		</script>
-		<noscript>
-			<p><img src="//piwik.openrailwaymap.org/piwik.php?idsite=6" style="border:0;" alt="" /></p>
-		</noscript>
-		<!-- End Piwik Code -->
 	</head>
 	<body>
 	<body id="background">
